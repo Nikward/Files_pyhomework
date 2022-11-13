c1a40dfd024a86b23f7c1ed3b36a0f72f3a6fb38
Nikward <114798709+Nikward@users.noreply.github.com>
2022-11-13 13:53:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,21 @@ def get_shop_list_by_dishes(dishes, person_count):
 total = get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
 print(cook_book, end="\n\n\n")
 print(total)
+
+def rewrite_sort_file(name_f1 = "1.txt", name_f2 = "2.txt", name_f3 = "3.txt"):
+    os.chdir("sorted") # Переходим в директорию папки sorted
+    rewrite_file = "rewrite_file.txt" # Наименование перезаписываемого файла
+    files = {}
+    for file in os.listdir(): # пробегаемся по директории с файлами и формируем словарь (название файла-количество строк)
+        with open(os.path.join(os.getcwd(), file), 'r', encoding = "utf-8") as f:
+            files[file] = len(f.readlines())
+
+    with open(os.path.join(os.getcwd(), rewrite_file), 'w', encoding='utf-8') as ff:
+        for i in sorted(files.items(), key=lambda para: para[1]):
+            ff.write(i[0] + "\n")
+            ff.write(str(i[1]) + "\n")
+            with open(os.path.join(os.getcwd(), i[0]), 'r', encoding="utf-8") as f:
+                file = f.readlines()
+            ff.writelines(file)
+                
+rewrite_sort_file()
